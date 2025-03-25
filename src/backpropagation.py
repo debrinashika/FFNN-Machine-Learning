48cead1d6c18e6cbe9772a5c87844891de12d72e
shika <debrina.veisha@gmail.com>
2025-03-25 14:38:22 +0700

    feat: add backpropagation

---
+++ src/backpropagation.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+# masih rusak
+def softmaxOutput(o, target):
+    o = np.array(o) 
+    target = np.array(target)
+    batch_size, num_neurons = o.shape
+    J = np.zeros((num_neurons, num_neurons))
+    
+    for i in range(num_neurons):
+        for j in range(num_neurons):
+            for b in range(batch_size):
+                J[i, j] = o[b, i] * (1 - o[b, j]) 
+
+    return J @ (-target / o)  
+
+def tanOutput(o, net, target):
+    target = np.array(target)  
+    turunan = 1 - np.tanh(net) ** 2
+    return (o - target) * turunan
+
+# masih rusak
+def softmaxHidden(w, o, delta):
+    o = np.array(o) 
+    batch_size, num_neurons = o.shape
+    J = np.zeros((num_neurons, num_neurons))
+
+    for i in range(num_neurons):
+        for j in range(num_neurons):
+            for b in range(batch_size):
+                J[i, j] = o[b, i] * (1 - o[b, j]) 
+
+    print("ini weight", w)
+    print("ini delta",delta)
+    print("ini j",J)
+
+    return np.dot(w,delta.T) 
+
+def tanHidden(w, o, delta):
+    # print("JAJAJ")
+    # print(delta)
+    # print(w)
+    return np.dot(w, delta) * o  
--- src/ffnn.py
+++ src/ffnn.py
@@ -1,5 +1,7 @@
 import numpy as np
-from layers import Layers  
+from layers import Layers 
+import activations 
+from backpropagation import softmaxOutput, softmaxHidden, tanOutput, tanHidden
 
 class FFNN:
     def __init__(self, batch_size: int, learning_rate: float, epoch: int, verbose: int, loss_func, weight_init, seed=None):
@@ -17,7 +19,7 @@ class FFNN:
         self.delta_gradien: list[np.ndarray] = []
 
     def initDeltaGradien(self):
-        self.delta_gradien = [np.zeros((layer.n_inputs, layer.n_neurons)) for layer in self.layers]
+        self.delta_gradien = [np.zeros((layer.n_inputs+1, layer.n_neurons)) for layer in self.layers]
 
     def initWeight(self, lower_bound=-0.5, upper_bound=0.5, mean=0.0, variance=1.0):
         if self.seed is not None:
@@ -35,7 +37,7 @@ class FFNN:
 
     def calcLoss(self, output: list[float], target: list[float]):
         if self.loss_func == "mse":
-            return np.mean((np.array(target) - np.array(output)) ** 2)
+            return np.mean((np.array(target) - np.array(output)) ** 2) / 2
         elif self.loss_func == "binary":
             pass
         elif self.loss_func == "categorical":
@@ -43,11 +45,12 @@ class FFNN:
 
     def updateWeight(self):
         for idx, layer in enumerate(self.layers):
-            layer.weight += self.delta_gradien[idx]
+            layer.weight += self.delta_gradien[idx][0]
 
     def updateGradien(self, layer_idx: int, delta: np.ndarray, input: np.ndarray):
-        grad = self.learning_rate * np.outer(input, delta)
-        self.delta_gradien[layer_idx] += grad
+        grad = self.learning_rate * np.dot(input, delta)
+        self.delta_gradien[layer_idx] = grad
+
 
     def addInputOutput(self, input: list[float], output: list[float]):
         self.input.append(input)
@@ -59,35 +62,65 @@ class FFNN:
     def feedForward(self):
         self.initWeight()
 
-        for i in range(1000):
-            if i == self.epoch:
+        for j in range(1000):
+            if j == self.epoch:
                 return
             
-            # self.initDeltaGradien()
+            self.initDeltaGradien()
             error = 0
             
-            for j, batch in enumerate(self.input):
+            for i, batch in enumerate(self.input):
+                inputs: list[list[float]] = []
+                nets: list[list[float]] = []
+
                 batch = np.array(batch)  
                 batch_size = batch.shape[0]
                 bias = np.ones((batch_size, 1))
                 current = np.hstack((bias, batch))  
+                inputs.append(current.copy().transpose().tolist())
 
                 for layer in self.layers:
-                    print("Sebelum aktivasi:", current)
-                    # print(layer.weight)
                     net = np.dot(current,layer.weight) 
                     current = layer.activ_func(net)
-                    print("Setelah aktivasi:", current)  
-
+                    nets.append(net.copy().transpose().tolist())
                     if layer != self.layers[-1]:
                         bias = np.ones((batch_size, 1))
                         current = np.hstack((bias, current))  
+                    inputs.append(current.copy().transpose().tolist())
 
-                # print(f"Target : {self.target[j]}")
-                # print(f"output : {current[j]}")
-                error += self.calcLoss(self.target[j],current[j]) 
-            
-            print(f"Epoch {i+1}, Loss: {error}")
+                error += self.calcLoss(self.target[i],current) 
+                print(f"Epoch {j+1}, Loss: {error}")
+                # print(inputs, "\n")
+                # print(nets)
+                self.backPropagation(inputs, nets, self.target[i])
+                self.updateWeight()
+                self.initDeltaGradien()
 
+    def backPropagation(self, inputs, netsLayer, target):
+        i = len(self.layers) - 1
+        delta1: np.ndarray = None
+        
+        while i >= 0:
+            nets = np.array(netsLayer[i]).T 
+   
+            if i == len(self.layers) - 1:  # Output layer
+                if self.layers[i].activ_func == activations.softmax:
+                    delta1 = softmaxOutput(inputs[i+1], target)
+                elif self.layers[i].activ_func == activations.tanh:
+                    delta1 = tanOutput(inputs[i+1], nets, target)  
+            else:  # Hidden layer
+                # print("LAYERR",self.layers[i + 1].weight)
+                if self.layers[i].activ_func == activations.softmax:
+                    delta2 = softmaxHidden(self.layers[i + 1].weight[1:], inputs[i+1][1:], delta1)  
+                elif self.layers[i].activ_func == activations.tanh:
+                    delta2 = tanHidden(self.layers[i + 1].weight[1:], inputs[i+1][1:], delta1)
+
+                self.updateGradien(i + 1, delta1, self.layers[i + 1].weight)
+                delta1 = delta2
+
+            i -= 1
+
+
+
+                
             
-           
